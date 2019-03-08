b45026aa0ce9bd9938cdd224a5184033ec4e22bf
KolbeinnIn <kolbeinnin264@nemi.tskoli.is>
2019-03-08 21:12:20 +0000

    Skil04

--- Skil04/Skil04.py
+++ Skil04/Skil04.py
@@ -1,16 +1,19 @@
-# Kolbeinn Ingólfsson
+# Ágúst, Bjarki, Kolbeinn
 # Skilaverkefni 4
 
 
 import socket
+import urllib.request
+
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 
 #dæmi 1 og 2
+#url = input("Sláðu inn vefslóð: ")
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
-
+print("Dæmi 1 og 2")
 while True:
     data = mysock.recv(1024).decode()
     count = len(data)
@@ -18,10 +21,21 @@ while True:
         data = data[:3000]
     if len(data) < 1:
         break
-    print(data)
+    print(data, end="")
     print("fjöldi stafa:", count)
 
 
 mysock.close()
 
 #Dæmi 3
+print("\nDæmi 3")
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+teljari = 0
+for line in fhand:
+    line = line.decode().strip()
+    if teljari > 3000:
+        break
+    print(line)
+    teljari += len(line)
+
+
